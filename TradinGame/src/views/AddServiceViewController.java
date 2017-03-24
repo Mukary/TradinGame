d2d131b7973091f4e36e9f36f2589baa258c5926
Max Cabourg <maxcabourg@etu.umontpellier.fr>
2017-03-24 09:53:46 +0100

    Add : Security on add service

--- TradinGame/src/views/AddServiceViewController.java
+++ TradinGame/src/views/AddServiceViewController.java
@@ -1,9 +1,13 @@
 package views;
 
+import java.time.LocalDate;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.Alert.AlertType;
 
 public class AddServiceViewController extends ViewController{
 	
@@ -18,12 +22,49 @@ public class AddServiceViewController extends ViewController{
 	
 	@FXML
 	private void handleAddServiceButton(){
+		Alert alert;
+		if(!validInputs()){
+			alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText(null);
+	        alert.initOwner(stage);
+	        alert.setTitle("Error");
+	        alert.setContentText("Please fill the required fields");
+	        alert.showAndWait();
+		}
+		else if(pickedDateBeforeNow()){
+			alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText(null);
+	        alert.initOwner(stage);
+	        alert.setTitle("Error");
+	        alert.setContentText("Please pick a date after the current date");
+	        alert.showAndWait();
+		}
+		else{
+			alert = new Alert(AlertType.CONFIRMATION);
+			alert.setHeaderText(null);
+	        alert.initOwner(stage);
+	        alert.setTitle("Service added");
+	        alert.setContentText("Your service has been well added");
+	        alert.showAndWait();
+		}
+			
 		
 	}
 	
 	@FXML
 	private void handleCancelButton(){
-		mainApp.showUserGeneralViewDialog();
+		stage.close();
 	}
 
+	private boolean validInputs(){	
+		LocalDate date = datePicker.getValue();
+		if(descriptionArea.getText().length() == 0 || date == null)
+			return false;
+		return true;
+	}
+	
+	private boolean pickedDateBeforeNow(){
+		LocalDate now = LocalDate.now();
+		return now.isAfter(datePicker.getValue());
+	}
 }
